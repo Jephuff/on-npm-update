45f997c6878a23f62e96e0388c72f6724b96eff3
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2018-02-17 12:11:31 -0500

    0.0.1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,45 @@
-var changes = require('concurrent-couch-follower')
+const changes = require('concurrent-couch-follower');
+const latestVersion = require('latest-version');
+const minimist = require('minimist');
+
+var { p: packageName, command } = minimist(process.argv.slice(2), {
+  string: ['package-name', 'command'],
+  alias: { p: 'package-name', c: 'command' },
+});
+
+if (!packageName) {
+  console.log('must provide a package name');
+  process.exit(1);
+}
+
+if (!command) {
+  console.log('must provide a command');
+  process.exit(1);
+}
+
+console.log('listening for changes of the ' + packageName);
+console.log('running "' + command + '" on change');
+
 const db = 'https://replicate.npmjs.com';
 
-var dataHandler = function(data, done) {
-    console.log(data.seq)
-    if(data.doc.name === 'on-npm-update') {
-        console.log(data.doc.name, data.doc['dist-tags'].latest);
+const configOptions = {
+  db,
+  include_docs: true,
+  sequence: '.sequence',
+  now: true,
+  concurrency: 1,
+};
+
+latestVersion(packageName).then(function(version) {
+  const currentVersion = version;
+
+  function onChange(data, done) {
+    if (data.doc.name === packageName) {
+      console.log(data.doc.name, data.doc['dist-tags'].latest);
     }
 
     done();
-}
+  }
 
-var configOptions = {
-    db,
-    include_docs: true,
-    sequence: '.sequence',
-    now: true,
-    concurrency: 1
-}
-
-changes(dataHandler, configOptions)
+  changes(onChange, configOptions);
+});
